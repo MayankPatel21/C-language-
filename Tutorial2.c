06ac5108246da59d82aa86964280c143fdccfc4c
MayankPatel21 <mayankptl2112@gmail.com>
2025-02-01 22:02:47 +0530

    Update Tutorial2.c

--- Tutorial2.c
+++ Tutorial2.c
@@ -2,7 +2,7 @@
 int main()
 {
   int a=2,b=3;
-  printf("The value of a is : ",a);
-  printf("The value of b is : ",b);
+  printf("The value of a is %d : ",a);
+  printf("The value of b is %d : ",b);
   return 0;
 }
