4b1af34f992e1dfa9fe71bf7f899072ca5cb0389
MayankPatel21 <mayankptl2112@gmail.com>
2025-02-05 23:02:15 +0530

    Create Tutorial7.c

---
+++ Tutorial7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  float a,b;
+  printf("Enter the value of a : ");
+  scanf("%f",&a);
+  printf("Enter the value of b : ");
+  scanf("%f",&b);
+
+  printf("The value of a is %f and b is %f ",a,b);
+  return 0;
+}
