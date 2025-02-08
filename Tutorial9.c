a04955e3685bf9393eac640ff98f1fdb9aec69e6
MayankPatel21 <mayankptl2112@gmail.com>
2025-02-08 16:16:53 +0530

    Create Tutorial9.c

---
+++ Tutorial9.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  char x,y;
+  printf("Enter any Character : ");
+  scanf("%c",&x);
+  printf("Enter any Character : ");
+  scanf("%c",&y);
+
+  printf("The Entered character is %c and %c ",x,y);
+  return 0;
+}
