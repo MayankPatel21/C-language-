6cad27a43c1abbc17a8ca01aaf65a3a5104c4efa
MayankPatel21 <mayankptl2112@gmail.com>
2025-02-10 21:55:42 +0530

    Create Tutorial11.c

---
+++ Tutorial11.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,sum;
+  printf("Enter the value of a : ");
+  scanf("%d",&a);
+
+  printf("Enter the value of b : ");
+  scanf("%d",&b);
+
+  printf("The value of a is : %d\n",a);
+  printf("The value of b is : %d\n",b);
+
+  sum=a+b;
+  printf("The sum is %d \n",sum);
+  return 0;
+}
