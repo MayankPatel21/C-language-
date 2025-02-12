a7e62a88e2b07b012d518a6c0ea518a9c8c4f083
MayankPatel21 <mayankptl2112@gmail.com>
2025-02-12 09:26:49 +0530

    Create Tutorial13.c

---
+++ Tutorial13.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+int main()
+{
+   int a,b,sub;
+   printf("Enter the value of a : ");
+   scanf("%d",&a);
+   
+   printf("Enter the value of b : ");
+   scanf("%d",&b);
+   
+   printf("The value of a is %d\n",a);
+   printf("The value of b is %d\n",b);
+   
+   sub=a-b;
+   printf("The sum is %d",sub);
+}
