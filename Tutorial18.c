3bc7a6409bf4b668197e57662b725f66ccf1a75d
MayankPatel21 <mayankptl2112@gmail.com>
2025-02-14 17:17:14 +0530

    Create Tutorial18.c

---
+++ Tutorial18.c
@@ -0,0 +1,14 @@
+int main()
+{
+    int a,b,c;
+    printf("Enter the value of a : ");
+    scanf("%d",&a);
+    printf("Enter the value of b : ");
+    scanf("%d",&b);
+    
+    printf("The value of a is %d\n",a);
+    printf("The value of b is %d\n",b);
+   
+    c=a/b;
+    printf("The Division is %d",c);
+}
