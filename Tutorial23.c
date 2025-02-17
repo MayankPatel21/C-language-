8eace38449a0f1e0d76ae530c87af5e17b8617ac
MayankPatel21 <mayankptl2112@gmail.com>
2025-02-17 16:55:49 +0530

    Create Tutorial23.c

---
+++ Tutorial23.c
@@ -0,0 +1,13 @@
+int main()
+{
+    float a,b;
+    printf("Enter the value of a : ");
+    scanf("%f",&a);
+    printf("Enter the value of b : ");
+    scanf("%f",&b);
+    
+    printf("The value of a is %f\n",a);
+    printf("The value of b is %f\n",b);
+   
+    printf("The Multiplication is %f",a*b);
+}
