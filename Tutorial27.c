58be7c407e9138c450b79eee34a66ecd88ac9667
MayankPatel21 <mayankptl2112@gmail.com>
2025-02-19 09:09:32 +0530

    Create Tutorial27.c

---
+++ Tutorial27.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+//AREA OF SUARE
+int main()
+{
+    
+    int l,area;
+    printf("Enter the length of square : ");
+    scanf("%d",&l);
+    
+    area = l*l;
+    printf("The area of square is : %d",area);
+    return 0;
+}
