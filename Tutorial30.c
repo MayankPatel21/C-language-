dce59357cc01f83162148c297004264b482de002
MayankPatel21 <mayankptl2112@gmail.com>
2025-02-22 08:51:00 +0530

    Create Tutorial30.c

---
+++ Tutorial30.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+//AREA OF RECTANGLE
+int main()
+{
+    
+    int l,b;
+    printf("Enter the length of Rectangle : ");
+    scanf("%d",&l);
+    printf("Enter the breadth of Rectangle : ");
+    scanf("%d",&b);
+    
+    printf("The area of Rectangle is : %d",l*b);
+    return 0;
+}
