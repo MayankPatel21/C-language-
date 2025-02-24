2646214ff0b0ca56a34dbcbc19a02264602651eb
MayankPatel21 <mayankptl2112@gmail.com>
2025-02-24 20:53:14 +0530

    Create Tutorial32.c

---
+++ Tutorial32.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+//AREA OF TRIANGLE
+int main()
+{
+    
+    int h,b,area;
+    printf("Enter the height of Triangle : ");
+    scanf("%d",&l);
+    printf("Enter the breadth of Triangle : ");
+    scanf("%d",&b);
+    
+    printf("The area of Triangle is : %d",(0.5*h*b));
+    return 0;
+}
