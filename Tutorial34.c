3c9b8002af0f07342fc1b5ced114cc7c24bd68d3
MayankPatel21 <mayankptl2112@gmail.com>
2025-02-27 20:38:32 +0530

    Create Tutorial34.c

---
+++ Tutorial34.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+//AREA OF CIRCLE
+int main()
+{
+    int r;
+    printf("Enter the radius of Circle : ");
+    scanf("%d",&r);
+    
+    printf("The area of Circle is : %f",(3.14*r*r));
+    return 0;
+}
