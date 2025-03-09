afb7ac4cef10c3849e110368bc9388f2b9722cdd
MayankPatel21 <mayankptl2112@gmail.com>
2025-03-09 14:40:30 +0530

    Create Tutorial36.c

---
+++ Tutorial36.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+//To check whether the number is negative, positive or zero
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num > 0) {
+        printf("The number is positive.\n");
+    } else if (num < 0) {
+        printf("The number is negative.\n");
+    } else {
+        printf("The number is zero.\n");
+    }
+
+    return 0;
+}
