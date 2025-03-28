b4e1922f794b11f64ed7c32b4ee5d7b03235a283
MayankPatel21 <mayankptl2112@gmail.com>
2025-03-28 21:34:55 +0530

    Create Tutorial78.c

---
+++ Tutorial78.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int number, guess, attempts = 0;
+
+    // Seed the random number generator
+    // The r and () function in c language generated random numbers
+    srand(time(0));
+    // rand() % 100 function gives the range between 0 to 99 .So +1 is necessary to shift integr or range
+    number = rand() % 100 + 1; // Random number between 1 and 100
+
+    printf("Welcome to the Number Guessing Game!\n");
+    printf("I have selected a number between 1 and 100. Try to guess it!\n");
+
+    // Loop until the correct number is guessed
+    do {
+        printf("Enter your guess: ");
+        scanf("%d", &guess);
+        attempts++;
+
+        if (guess > number) {
+            printf("Too high! Try again.\n");
+        } else if (guess < number) {
+            printf("Too low! Try again.\n");
+        } else {
+            printf("Congratulations! You guessed the number in %d attempts.\n", attempts);
+        }
+    } while (guess != number);
+
+    return 0;
+}
