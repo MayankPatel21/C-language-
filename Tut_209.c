ff5a3df02d1d1969ecbf310874d241839a9657df
MayankPatel21 <mayankptl2112@gmail.com>
2025-05-13 22:10:15 +0530

    Create Tut_209.c

---
+++ Tut_209.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int month;
+    printf("Enter month number (1-12): ");
+    scanf("%d", &month);
+
+    if (month == 12 || month == 1 || month == 2) {
+        printf("Season: Winter\n");
+    } else if (month >= 3 && month <= 5) {
+        printf("Season: Spring\n");
+    } else if (month >= 6 && month <= 8) {
+        printf("Season: Summer\n");
+    } else if (month >= 9 && month <= 11) {
+        printf("Season: Autumn\n");
+    } else {
+        printf("Invalid month number.\n");
+    }
+
+    return 0;
+}
