ba04083dddcb1595c326c9f6db6e74c8f3a86731
MayankPatel21 <mayankptl2112@gmail.com>
2025-05-31 22:35:13 +0530

    Create Tut_257.c

---
+++ Tut_257.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    char str[] = "Beautiful day ahead!";
+    int count = 0;
+
+    for(int i = 0; str[i] != '\0'; i++) {
+        char ch = str[i];
+        if(ch == 'a' || ch == 'e' || ch == 'i' ||
+           ch == 'o' || ch == 'u' ||
+           ch == 'A' || ch == 'E' || ch == 'I' ||
+           ch == 'O' || ch == 'U') {
+            count++;
+        }
+    }
+
+    printf("Number of vowels: %d\n", count);
+    return 0;
+}
